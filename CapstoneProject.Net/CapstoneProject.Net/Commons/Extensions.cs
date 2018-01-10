ca1e0d9702ea0f4b6bc9c5c119c21c7c1a93f230
BAOTDSE62099\Temporary <baokeylog@gmail.com>
2018-01-11 00:52:54 +0700

    Init project

---
+++ CapstoneProject.Net/CapstoneProject.Net/Commons/Extensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace CapstoneProject.Net.Commons
+{
+    public static class Extensions
+    {
+        public static string GetEnumDescription(this Enum value)
+        {
+            Type type = value.GetType();
+
+            MemberInfo[] memInfo = type.GetMember(value.ToString());
+
+            if (memInfo != null && memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+
+            return value.ToString();
+        }
+
+        public static string DisplayName(this Enum value)
+        {
+            try
+            {
+                Type enumType = value.GetType();
+                var enumValue = Enum.GetName(enumType, value);
+                MemberInfo member = enumType.GetMember(enumValue)[0];
+
+                var attrs = member.GetCustomAttributes(typeof(DisplayAttribute), false);
+                var outString = ((DisplayAttribute)attrs[0]).Name;
+
+                if (((DisplayAttribute)attrs[0]).ResourceType != null)
+                {
+                    outString = ((DisplayAttribute)attrs[0]).GetName();
+                }
+
+                return outString;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ CapstoneProject.Net/CapstoneProject.Net/Commons/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.Net.Commons
+{
+    public static class Utils
+    {
+        public static bool IsDigitsOnly(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
